bcc81d3e1ee9de11317641172cce9bfa55af6e7e
mnrubin <mnrubin@optonline.net>
2012-11-11 22:51:41 -0500

    added module conenctor

---
+++ Modules/src/ModuleConnector.java
@@ -0,0 +1,16 @@
+import java.io.File;
+
+
+public class ModuleConnector {
+
+	/**
+	 * Finds connections between Modules.  reads them in from text files (?)
+	 * @param args
+	 */
+	public ModuleConnector(String filename)
+	{
+		File f = new File(filename+".txt");
+		
+	}
+
+}
