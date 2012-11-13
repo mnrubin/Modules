bc85c8af456e6e27571d96fc51a97db27915de68
mnrubin <mnrubin@optonline.net>
2012-11-13 18:26:19 -0500

    modgraph tester

--- Modules/src/ModGraph.java
+++ Modules/src/ModGraph.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -5,7 +6,13 @@ import java.util.List;
 
 public class ModGraph {
 	List<Node> allNodes;
+	ArrayList<String> nodesForLookup;
+	int[][] result;
+	
 	public ModGraph(LinkedList<Pair> list, ArrayList<String> nodes, int[][] result) {
+		allNodes = new LinkedList<Node>();
+		nodesForLookup = nodes;
+		this.result = result;
 		for (Pair p:list) {
 			Node pre_node = null;
 			Node post_node = null;
@@ -32,14 +39,74 @@ public class ModGraph {
 			if (pre_node.out_nodes == null) {
 				pre_node.out_nodes = new LinkedList<Node>();
 			}
-			if (pre_node.in_nodes == null) {
-				pre_node.in_nodes = new LinkedList<Node>();
+			if (post_node.in_nodes == null) {
+				post_node.in_nodes = new LinkedList<Node>();
 			}
-			if (!pre_node.out_nodes.contains(post_node) && !post_node.in_nodes.contains(pre_node)) {
+			if ((pre_node.out_nodes == null || !pre_node.out_nodes.contains(post_node)) && (post_node.in_nodes == null || !post_node.in_nodes.contains(pre_node))) {
 				pre_node.out_nodes.add(post_node);
 				post_node.in_nodes.add(pre_node);
 			}
 			
 		}
+	}//end constructor
+
+	public boolean[][] getAdjMatrix()
+	{
+		boolean[][] adjMatrix = new boolean[nodesForLookup.size()][nodesForLookup.size()];
+		for(Node n : allNodes)
+		{
+			List<Node> out_list = n.out_nodes;
+			for(Node outN : out_list)
+			{
+				int pre_index = nodesForLookup.indexOf(n);
+				int post_index = nodesForLookup.indexOf(outN);
+				if(result[pre_index][post_index] >0)
+				{
+					adjMatrix[pre_index][post_index] = true;
+				}
+			}
+		}
+		
+		return adjMatrix;
 	}
-}
+
+	public List<String> getNodesForLookup()
+	{
+		return nodesForLookup;
+	}
+	
+	public static void main(String args[]) throws IOException
+	{
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("a");
+		list.add("b");
+		list.add("c");
+		list.add("d");
+		Pair p1 = new Pair("a", "b");
+		Pair p2 = new Pair("b", "c");
+		Pair p3 = new Pair("b", "d");
+		LinkedList<Pair> pairs = new LinkedList<Pair>();
+		pairs.add(p1);
+		pairs.add(p2);
+		pairs.add(p3);
+		int[][] result = new int[list.size()][list.size()];
+		result[0][1] = 5;
+		result[1][2] = 2;
+		result[1][3] = 4;
+		ModGraph mg = new ModGraph(pairs, list, result);
+		boolean[][] matrix = mg.getAdjMatrix();
+		for(int i = 0; i < list.size(); i++)
+		{
+			for(int j = 0; j < list.size(); j++)
+			{
+				System.out.print(matrix[i][j] + " ");
+			}
+			System.out.println("\n");
+		}
+		
+	}
+
+}//end class
+
+
+
