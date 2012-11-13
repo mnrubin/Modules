399ce0015405e3ff7a92130087dc6d4f725fb935
mnrubin <mnrubin@optonline.net>
2012-11-13 18:33:08 -0500

    ,

--- Modules/src/Node.java
+++ Modules/src/Node.java
@@ -1,3 +1,4 @@
+import java.util.LinkedList;
 import java.util.List;
 
 
@@ -5,4 +6,11 @@ public class Node {
 	public String name;
 	public List<Node> in_nodes;
 	public List<Node> out_nodes;
+	
+	public Node(String name)
+	{
+		in_nodes = new LinkedList<Node>();
+		out_nodes = new LinkedList<Node>();
+		this.name = name;
+	}
 }
